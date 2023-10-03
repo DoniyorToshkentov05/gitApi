a8cb9b78303839931b283a24bc2b9e1522575d19
Doniyortoshkentov05 <doniyortoshkentov@gmail.com>
2023-10-04 02:16:13 +0500

    end

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-const fetchDataBtn = document.querySelector("#fetchdata");
-const result = document.querySelector("#result");
-const input = document.querySelector("input");
+let url = " https://api.github.com/users";
 
+const search = document.querySelector("#search");
 
+const form = document.querySelector("#form");
 
+const btn = document.querySelector(".btn");
 
-const getData = function () {
-  result.innerText = "Loading...";
-  fetch(`https://api.github.com/users/${input.value}`)
+const card = document.querySelector(".cards");
+
+search.addEventListener("keyup", (e) => {
+  let url = ` https://api.github.com/users/${e.target.value}`;
+  fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      result.innerText = JSON.stringify(data, null, 2);
-    })
-   
-};
 
-
-fetchDataBtn.addEventListener("click", getData);
+      card.innerHTML = `<li class="list">
+      <img src="${data.avatar_url}" alt="" />
+      <h2 class="h2">Nomi:     ${e.target.value}</h2>
+      <a href="${data.html_url}"><button class="btn1">GitHub Havola</button></a>
+      <p class="p3"></p>
+    </li>`;
+    
+    });
+});
